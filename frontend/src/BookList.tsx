08610aa267458b06d47e68a392a15a28535325d6
IcyThunder22 <153141281+IcyThunder22@users.noreply.github.com>
2025-03-21 20:15:22 -0600

    Basically the whole thing :)

--- frontend/src/BookList.tsx
+++ frontend/src/BookList.tsx
@@ -1,49 +1,114 @@
-import { useEffect, useState } from 'react';
-import { Book } from './types/Book';
+import { useEffect, useState } from "react";
+import { Book } from "./types/Book";
 
 function BooksList() {
   const [Books, setBooks] = useState<Book[]>([]);
+  const [pageSize, setPageSize] = useState<number>(5);
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [totalBooks, setTotalBooks] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [sortByTitle, setSortByTitle] = useState(false);
 
   useEffect(() => {
     const fetchBook = async () => {
-      const response = await fetch('https://localhost:7072/Books');
+      const response = await fetch(
+        `https://localhost:7072/Books?pageNum=${pageNum}&pageSize=${pageSize}&sort=${sortByTitle}`
+      );
       const data = await response.json();
-      setBooks(data);
+      setBooks(data.books);
+      setTotalBooks(data.total);
+      setTotalPages(Math.ceil(totalBooks / pageSize));
     };
     fetchBook();
-  }, []);
+  }, [pageSize, pageNum, sortByTitle]);
 
   return (
     <>
-      <h1>Books List</h1>
-      <table>
-        <thead>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Publisher</th>
-          <th>ISBN</th>
-          <th>Classification</th>
-          <th>Category</th>
-          <th>PageCount</th>
-          <th>Price</th>
-        </thead>
-        <tbody>
-          {Books.map((b) => (
-            <tr key={b.bookID}>
-              <td>
-                {b.title}
-              </td>
-              <td>{b.author}</td>
-              <td>{b.publisher}</td>
-              <td>{b.isbn}</td>
-              <td>{b.classification}</td>
-              <td>{b.category}</td>
-              <td>{b.pageCount}</td>
-              <td>{b.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1>Book List</h1>
+
+      <br />
+      <button onClick={() => setSortByTitle(!sortByTitle)}>
+        {sortByTitle ? "Unsort by Title" : "Sort by Title"}
+      </button>
+      <br />
+
+      {Books.map((b) => (
+        <div id="bookID" className="card" key={b.bookID}>
+          <h3 className="card-title">{b.title}</h3>
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li>
+                <strong>Author: </strong>
+                {b.author}
+              </li>
+              <li>
+                <strong>Publisher: </strong>
+                {b.publisher}
+              </li>
+              <li>
+                <strong>ISBN: </strong>
+                {b.isbn}
+              </li>
+              <li>
+                <strong>Classification: </strong>
+                {b.classification}
+              </li>
+              <li>
+                <strong>Category: </strong>
+                {b.category}
+              </li>
+              <li>
+                <strong>Page Count: </strong>
+                {b.pageCount}
+              </li>
+              <li>
+                <strong>Price: </strong>
+                {b.price}
+              </li>
+            </ul>
+          </div>
+        </div>
+      ))}
+
+      <br />
+
+      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>
+        Previous
+      </button>
+
+      {[...Array(totalPages)].map((_, i) => (
+        <button
+          key={i + 1}
+          onClick={() => setPageNum(i + 1)}
+          disabled={pageNum === i + 1}
+        >
+          {i + 1}
+        </button>
+      ))}
+
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setPageNum(pageNum + 1)}
+      >
+        Next
+      </button>
+
+      <br />
+
+      <label>
+        Results per page:
+        <select
+          value={pageSize}
+          onChange={(p) => {
+            setPageSize(Number(p.target.value));
+            setPageNum(1);
+          }}
+        >
+          <option value="2">2</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+        </select>
+      </label>
     </>
   );
 }
